087932bd3258626cdf6fe470f4e746a5fb297f7c
Uladzimir Davidziuk <David.Vladimir95@gmail.com>
2026-01-09 23:13:41 +0100

    Initial commit

---
+++ src/main/java/pl/uldav/pgprototype/PgprototypeApplication.java
@@ -0,0 +1,13 @@
+package pl.uldav.pgprototype;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PgprototypeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PgprototypeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/uldav/pgprototype/PgprototypeApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.uldav.pgprototype;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PgprototypeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
